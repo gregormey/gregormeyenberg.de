901037155095692a53e2a92aea049a6fe3a250bf
Gregor Meyenberg <gregor@meyenberg.de>
2014-02-02 11:29:23 +0100

    using classes for player and ball

--- pong/js/pong.js
+++ pong/js/pong.js
@@ -3,6 +3,86 @@
 	{
 		settings = $.extend({}, $.fn.pong.defaults, settings);
 		
+		var Player = function(elem){
+			this.x=5;
+			this.y=15;
+			this.move=false;
+			this.width=10;
+			this.height=40;
+			this.speed=1;
+			this.direct=0;
+			this.elem=elem;
+			var ctx=elem.getContext('2d');
+			this.hit=function(){
+				if((ctx.ball.x-ctx.ball.radius) < 
+								(this.x+this.width)
+							&&
+							(ctx.ball.y-ctx.ball.radius) > this.y &&
+							(ctx.ball.y+ctx.ball.radius) < (this.y+this.height)
+						)
+							return -((this.y+(this.height/2))-ctx.ball.y)/10;
+						else
+							return false;
+			};
+
+			this.move=function(){
+				h=$(this.elem).innerHeight();
+				var inc=this.speed*this.direct;
+				if((this.y+inc)<(h-this.height)
+					&& (this.y+inc)>0
+				){
+					ctx.clearRect(0,0,this.width+6,h);
+					ctx.player.y+=inc;
+					ctx.fillRect(this.x,this.y,this.width,this.height);
+				}
+					this.speed+=0.2;
+			};
+		};
+
+		var Ball=function(elem){
+			this.x=100;
+			this.y=100;
+			this.move=false;
+			this.radius=5;
+			this.elem=elem;
+			var ctx=elem.getContext('2d');
+			this.move=function(incX,incY){
+				ctx.clearRect(this.x-this.radius,this.y-this.radius,this.radius*2,this.radius*2);
+				h=$(this.elem).innerHeight();
+				w=$(this.elem).innerWidth();
+				
+			
+				if((this.y+incY)>(h-this.radius) ||
+					(this.y+incY)<(this.radius)){
+					incY=incY*-1;	
+				}
+				
+				if((this.x+incX)>(w-this.radius) ||
+					(this.x+incX)<(this.radius)){
+					incX=incX*-1;	
+				}
+				
+				this.x+=incX;
+				this.y+=incY;
+				var hit=ctx.player.hit();
+				if(hit)
+		        {	
+		        	incY=hit;
+		        	incX=incX*-1;
+		        	this.x+=incX;
+					this.y+=incY;	
+		        }
+				ctx.beginPath();
+		        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,true);// Outer circle
+		        ctx.fill();
+		        
+		        window.setTimeout("$.fn.pong.ctx['"+this.elem.id+"'].ball.move("+incX+","+incY+")",1);
+			};
+
+		};
+
+		
+
 		return this.each(
 			function()
 			{
@@ -11,46 +91,23 @@
 				var ctx = me.getContext('2d');
 				ctx.fillStyle = $me.css("color");
 				ctx.strokeStyle=  $me.css("color"); 
-				ctx.player={
-					"x":5,
-					"y":15,
-					"move":false,
-					"width":10,
-					"height":40,
-					"speed"	:1,
-					"direct":0,
-					"hitBall":function(){
-						if((ctx.ball.x-ctx.ball.radius) < 
-								(ctx.player.x+ctx.player.width)
-							&&
-							(ctx.ball.y-ctx.ball.radius) > ctx.player.y &&
-							(ctx.ball.y+ctx.ball.radius) < (ctx.player.y+ctx.player.height)
-						)
-							return -((ctx.player.y+(ctx.player.height/2))-ctx.ball.y)/10;
-						else
-							return false;
-							
-					}
-				};
-				
-				ctx.ball={
-					"x":100,
-					"y":100,
-					"move":false,
-					"radius":5	
-				};
+				ctx.player=new Player(this);
+				ctx.opponent=new Player(this);
+				ctx.ball=new Ball(this);
+				$.fn.pong.ctx[me.id]=ctx;
+
 				
 				$(document).keydown(
 					function(event){
 						if(event.keyCode==38){
 							if(!ctx.move){
 								ctx.player.direct=-1;
-								ctx.move=window.setInterval("$.fn.pong.movePlayer('"+me.id+"')",1);
+								ctx.move=window.setInterval("$.fn.pong.ctx['"+me.id+"'].player.move();",1);
 							}
 						}else if(event.keyCode==40){
 							if(!ctx.move){	
 								ctx.player.direct=1;
-								ctx.move=window.setInterval("$.fn.pong.movePlayer('"+me.id+"')",1);
+								ctx.move=window.setInterval("$.fn.pong.ctx['"+me.id+"'].player.move();",1);
 							}
 						}
 					}
@@ -69,62 +126,14 @@
     			 ctx.beginPath();
           		 ctx.arc(ctx.ball.x,ctx.ball.y,ctx.ball.radius,0,Math.PI*2,true);// Outer circle
          			ctx.fill();
-         		window.setTimeout("$.fn.pong.moveBall('"+me.id+"',-1,0)",1);
+         		window.setTimeout("$.fn.pong.ctx['"+me.id+"'].ball.move(-1,0)",1);
 				//Ausfallswinkel = (x*Padgeschwindigkeit+(180°-Einfallswinkel);
     			//ctx.clearRect(45,45,60,60);  
     			//ctx.strokeRect(50,50,50,50);	
 			});
 	}	
 	$.fn.pong.defaults = {};
-	$.fn.pong.moveBall=function(id,incX,incY){
-		elem=$("#"+id)[0];
-		ctx=elem.getContext('2d');
-		ctx.clearRect(ctx.ball.x-ctx.ball.radius,ctx.ball.y-ctx.ball.radius,ctx.ball.radius*2,ctx.ball.radius*2);
-		h=$(elem).innerHeight();
-		w=$(elem).innerWidth();
-		
-	
-		if((ctx.ball.y+incY)>(h-ctx.ball.radius) ||
-			(ctx.ball.y+incY)<(ctx.ball.radius)){
-			incY=incY*-1;	
-		}
-		
-		if((ctx.ball.x+incX)>(w-ctx.ball.radius) ||
-			(ctx.ball.x+incX)<(ctx.ball.radius)){
-			incX=incX*-1;	
-		}
-		
-		ctx.ball.x+=incX;
-		ctx.ball.y+=incY;
-		var hit=ctx.player.hitBall();
-		if(hit)
-        {	
-        	incY=hit;
-        	incX=incX*-1;
-        	ctx.ball.x+=incX;
-			ctx.ball.y+=incY;	
-        }
-		ctx.beginPath();
-        ctx.arc(ctx.ball.x,ctx.ball.y,ctx.ball.radius,0,Math.PI*2,true);// Outer circle
-        ctx.fill();
-        
-        window.setTimeout("$.fn.pong.moveBall('"+id+"',"+incX+","+incY+")",1);
-	};
-		
-	$.fn.pong.movePlayer=function(id,direct){
-		elem=$("#"+id)[0];
-		ctx=elem.getContext('2d');
-		h=$(elem).innerHeight();
-		var inc=ctx.player.speed*ctx.player.direct;
-		if((ctx.player.y+inc)<(h-ctx.player.height)
-		&& (ctx.player.y+inc)>0
-		){
-			ctx.clearRect(0,0,ctx.player.width+6,h);
-			ctx.player.y+=inc;
-			ctx.fillRect(ctx.player.x,ctx.player.y,ctx.player.width,ctx.player.height);
-		}
-		ctx.player.speed+=0.2;
-	};
-	
+	$.fn.pong.ctx={};
+
 
 })(jQuery)
