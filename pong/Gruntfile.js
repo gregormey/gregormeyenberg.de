16eccb972e4f85fb16b330b3fbae7767142ada2f
Gregor Meyenberg <gregor@meyenberg.de>
2014-02-04 22:18:18 +0100

    use grunt as build system

---
+++ pong/Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      my_target:{
+        files: {
+         'js/<%= pkg.name %>.min.js':['js/src/class/Ball.js',
+                                        'js/src/class/ComputerPlayer.js',
+                                        'js/src/class/HumanPlayer.js',
+                                        'js/src/class/Player.js',
+                                        'js/src/plugin.js']
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
