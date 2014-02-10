20176512b102758ddcc0de7984dce41801f4d749
Gregor Meyenberg <gregor@meyenberg.de>
2014-02-10 21:03:27 +0100

    Comments for Ball class

--- pong/js/src/class/Ball.js
+++ pong/js/src/class/Ball.js
@@ -1,13 +1,51 @@
+/**
+ * Ball class, handles the Ball movements
+ * @param {HTMLCanvasElement} elem
+ */
 var Ball=function(elem){
+	/**
+	 * initial x coordinate
+	 * @type {Number}
+	 */
 	this.x=100;
+	/**
+	 * initial y coordinate
+	 * @type {Number}
+	 */
 	this.y=100;
+	/**
+	 * Ball move speed
+	 * @type {Number}
+	 */
 	this.speed=1;
+	/**
+	 * Move interval handle
+	 * @type {Boolean}
+	 */
 	this.move=false;
+	/**
+	 * Ball radius size
+	 * @type {Number}
+	 */
 	this.radius=5;
+	/**
+	 * Canvas Element
+	 * @type {HTMLCanvasElement}
+	 */
 	this.elem=elem;
 
+	/**
+	 * Rendering Context
+	 * @type {CanvasRenderingContext2D}
+	 */
 	var ctx=elem.getContext('2d');
 
+	/**
+	 * Draws Ball at current position and triggers move interval
+	 * @param  {Number} incX
+	 * @param  {Number} incY
+	 * @return {Null}
+	 */
 	this.draw=function(incX,incY){
 		ctx.beginPath();
         ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,true);// Outer circle
@@ -17,8 +55,18 @@ var Ball=function(elem){
 
 	};
 
+	/**
+	 * initian callof the draw method
+	 */
 	this.draw(-2,0);
 
+	/**
+	 * Sets next position  of Ball by given direction parameters (incX,incY) 
+	 * and checks if the ball hits a player or the outline
+	 * @param  {[type]} incX
+	 * @param  {[type]} incY
+	 * @return {[type]}
+	 */
 	this.move=function(incX,incY){
 		ctx.clearRect(this.x-this.radius,this.y-this.radius-1,this.radius*2+ 2,this.radius*2+ 2);
 		h=$(this.elem).innerHeight();
