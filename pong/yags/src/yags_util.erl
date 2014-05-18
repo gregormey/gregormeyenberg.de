ad985e2d3df3cc39cf5e4c8819c65cccc4c466dd
Gregor Meyenberg <gregor@meyenberg.de>
2014-05-18 14:07:55 +0200

    spec definition for yags_util.erl

--- pong/yags/src/yags_util.erl
+++ pong/yags/src/yags_util.erl
@@ -5,13 +5,17 @@
 -export([validate/2]).
 -export([validate/3]).
 
+-type validation_result() :: valid | nick_not_valid | password_not_valid | mail_not_valid.
+
 %% generates a POSIX timestamp
+-spec unixTS()-> non_neg_integer().
 unixTS()->
 	{Mega, Secs, _} = os:timestamp(),
 	Mega*1000000 + Secs.
 
 %% validates a email
 %% standard HTML5 secified regex is used
+-spec validate(email|password|nick,string())-> valid | not_valid.
 validate(email,Sting)->
 	case re:run(Sting, "\\b[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*\\b") of
 		nomatch -> not_valid;
@@ -30,6 +34,7 @@ validate(nick,String)->
 		false -> not_valid
 	end.
 %% wrapper to validate input for account creation
+-spec validate(Nick::string(),Password::string(),Mail::string())-> validation_result().
 validate(Nick,Password, Mail) ->
 	case {validate(nick,Nick),validate(password,Password),validate(email,Mail)} of
 		{valid,valid,valid} -> valid;
